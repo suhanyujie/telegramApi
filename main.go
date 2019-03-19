39ec355fe0f5853f7952b90f3723dcf4cf16ca11
suhanyujie <suhanyujie@qq.com>
2019-03-19 17:06:39 +0800

    调整包名

--- main.go
+++ main.go
@@ -1,10 +1,10 @@
-package main
+package telegramApi
 
 import "telegramApi/apiImpl"
 
 // 尝试发送telegram的消息给机器人
 // https://core.telegram.org/bots/api#available-methods
-func main() {
+func test() {
 	//fmt.Println(configData.BaseUrl)
 	// 尝试发送markdown格式的数据
 	mdCon := "```\n " +
